12e77a61d755c8d878c2170b5d44d91636abf3d2
Jason Daggs <jdaggs@powercosts.com>
2019-08-27 08:53:02 -0500

    Initial Code Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
+// Image Classifier
+
 import React from 'react';
-import logo from './logo.svg';
+import * as tf from '@tensorflow/tfjs';
+import * as mobilenet from '@tensorflow-models/mobilenet'; 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props)
+    {
+        super(props)
+        this.state = { status: "Loading"};
+    }
+
+    componentDidMount()
+    {
+        const loadModel = async () => { 
+            const model = await mobilenet.load();
+            const imageEl = document.getElementById('img');
+            const result = await model.classify(imageEl);
+            const labels = result[0].className + " " + result[1].className + " " + result[2].className;
+            this.setState({status: labels});
+        }
+        loadModel(); 
+    }
+
+    render() 
+    {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img crossOrigin="anonymous" width='320' height='240' id='img'src = 'https://media.istockphoto.com/photos/snail-picture-id511478884' />
+           {this.state.status} 
+          </header>
+        </div>
+      );
+    }
 }
 
 export default App;
